f36505b90610747ed13da96cb77995cc7cceee4a
Rashmi Raj <rashmiraj7877@gmail.com>
2024-07-08 19:06:25 +0530

    For indentation, changes use tabs to space

--- 06_components/03_modal-using-css-and-js/main.js
+++ 06_components/03_modal-using-css-and-js/main.js
@@ -7,54 +7,54 @@ const field = input.parentElement;
 
 // Function to show the modal
 const handleShowModal = () => {
-	popup.classList.add('show');
+  popup.classList.add('show');
 };
 
 // Function to close the modal
 const handleCloseModal = () => {
-	popup.classList.remove('show');
+  popup.classList.remove('show');
 };
 
 // Function to close the modal by clicking outside the modal
 const handleOutsideClick = (e) => {
-	const isClickedInsideModal = e.target.closest('.popup');
-	if (!isClickedInsideModal && e.target !== viewBtn) {
-		handleCloseModal();
-	}
+  const isClickedInsideModal = e.target.closest('.popup');
+  if (!isClickedInsideModal && e.target !== viewBtn) {
+    handleCloseModal();
+  }
 };
 
 // Function to copy the input field value to clipboard
 const handleCopyToClipboard = async () => {
-	try {
-		input.select();
-		await navigator.clipboard.writeText(input.value);
+  try {
+    input.select();
+    await navigator.clipboard.writeText(input.value);
 
-		// Change the layout of copy button
-		copy.innerText = 'Copied';
-		copy.style.backgroundColor = 'var(--telegram-color)';
+    // Change the layout of copy button
+    copy.innerText = 'Copied';
+    copy.style.backgroundColor = 'var(--telegram-color)';
 
-		// Highlight the copied text
-		field.classList.add('active');
+    // Highlight the copied text
+    field.classList.add('active');
 
-		// Remove the change after 3 seconds
-		setTimeout(() => {
-			window.getSelection().removeAllRanges();
-			copy.innerText = 'Copy';
-			copy.style.backgroundColor = 'var(--background-color)';
+    // Remove the change after 3 seconds
+    setTimeout(() => {
+      window.getSelection().removeAllRanges();
+      copy.innerText = 'Copy';
+      copy.style.backgroundColor = 'var(--background-color)';
 
-			field.classList.remove('active');
-		}, 3000);
-	} catch (err) {
-		console.log(err);
-	}
+      field.classList.remove('active');
+    }, 3000);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // Event Listener Functions
 const handleInit = () => {
-	viewBtn.addEventListener('click', handleShowModal);
-	close.addEventListener('click', handleCloseModal);
-	document.addEventListener('click', handleOutsideClick);
-	copy.addEventListener('click', handleCopyToClipboard);
+  viewBtn.addEventListener('click', handleShowModal);
+  close.addEventListener('click', handleCloseModal);
+  document.addEventListener('click', handleOutsideClick);
+  copy.addEventListener('click', handleCopyToClipboard);
 };
 
 document.addEventListener('DOMContentLoaded', handleInit);
--- 07_omnifood/js/main.js
+++ 07_omnifood/js/main.js
@@ -11,51 +11,51 @@ yearElement.textContent = currentYear;
 // Make mobile navigation work
 
 btnNavElement.addEventListener('click', () => {
-	headerElement.classList.toggle('nav-open');
+  headerElement.classList.toggle('nav-open');
 });
 
 // Add smooth scrolling to navigation links (Needed for older version of browsers)
 allLinks.forEach((link) => {
-	link.addEventListener('click', (e) => {
-		e.preventDefault();
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
 
-		const href = link.getAttribute('href');
+    const href = link.getAttribute('href');
 
-		// Scroll back to top
-		if (href === '#') {
-			window.scrollTo({ top: 0, behavior: 'smooth' });
-		}
+    // Scroll back to top
+    if (href === '#') {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
 
-		// Scroll to other links
-		if (href !== '#' && href.startsWith('#')) {
-			const sectionElement = document.querySelector(href);
-			sectionElement.scrollIntoView({ behavior: 'smooth' });
-		}
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionElement = document.querySelector(href);
+      sectionElement.scrollIntoView({ behavior: 'smooth' });
+    }
 
-		// Close mobile navigation
-		if (link.classList.contains('main-nav-link')) {
-			headerElement.classList.toggle('nav-open');
-		}
-	});
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link')) {
+      headerElement.classList.toggle('nav-open');
+    }
+  });
 });
 
 // Sticky Navigation
 
 const obsHelper = (entries) => {
-	const ent = entries[0];
+  const ent = entries[0];
 
-	if (ent.isIntersecting === false) {
-		document.body.classList.add('sticky');
-	}
+  if (ent.isIntersecting === false) {
+    document.body.classList.add('sticky');
+  }
 
-	if (ent.isIntersecting === true) {
-		document.body.classList.remove('sticky');
-	}
+  if (ent.isIntersecting === true) {
+    document.body.classList.remove('sticky');
+  }
 };
 const obs = new IntersectionObserver((entries) => obsHelper(entries), {
-	root: null,
-	threshold: [0],
-	rootMargin: '-80px',
+  root: null,
+  threshold: [0],
+  rootMargin: '-80px',
 });
 
 obs.observe(sectionHeroElement);
